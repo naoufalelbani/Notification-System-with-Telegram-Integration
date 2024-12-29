c6024386ff96481118c8eb4f322ca0bece40a7dd
Naoufal <elbaninaoufal@gmail.com>
2024-12-29 23:38:30 +0100

    add clear pytest_cache to invoke tasks

--- tasks.py
+++ tasks.py
@@ -4,18 +4,21 @@ from invoke import task
 def run(c):
     """Run the main Python script and clear cache before execution."""
     c.run("find /workspace -type d -name '__pycache__' -exec rm -rf {} +")
+    c.run("find /workspace -type d -name '.pytest_cache' -exec rm -rf {} +")
     c.run("python ./src/main.py")
 
 @task
 def test(c):
     """Run Python tests and clear cache before execution."""
-    c.run("find /workspace -type d -name '__pycache__' -exec rm -rf {} +")
+    c.run("find /workspace -type d -name '__pycache__' -exec rm -rf {} +") 
+    c.run("find /workspace -type d -name '.pytest_cache' -exec rm -rf {} +")
     c.run("pytest")
 
 @task
 def clear_cache(c):
     """Clear all __pycache__ directories."""
     c.run("find /workspace -type d -name '__pycache__' -exec rm -rf {} +")
+    c.run("find /workspace -type d -name '.pytest_cache' -exec rm -rf {} +")
 
 @task
 def update_pip(c):
